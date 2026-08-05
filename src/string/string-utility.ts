2dbb51a93940c2638e68adae83dea088c76c7995
blwatkins <blwatkins@users.noreply.github.com>
2026-08-04 22:06:41 -0500

    Update string module TSDocs.

--- src/string/string-utility.ts
+++ src/string/string-utility.ts
@@ -33,7 +33,7 @@ export class StringUtility {
     /**
      * Private constructor.
      *
-     * @throws {Error} - StringUtility is a static class and cannot be instantiated.
+     * @throws {Error} StringUtility is a static class and cannot be instantiated.
      *
      * @private
      */
@@ -44,9 +44,9 @@ export class StringUtility {
     /**
      * Get the regular expression for single-line lowercase strings.
      *
-     * @remarks - This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
+     * @remarks This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
      *
-     * @returns {RegExp} - Regular expression pattern for validating single-line lowercase strings.
+     * @returns {RegExp} Regular expression pattern for validating single-line lowercase strings.
      *
      * @public
      * @since 0.1.0
@@ -58,9 +58,9 @@ export class StringUtility {
     /**
      * Get the regular expression for single-line uppercase strings.
      *
-     * @remarks - This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
+     * @remarks This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
      *
-     * @returns {RegExp} - Regular expression pattern for validating single-line uppercase strings.
+     * @returns {RegExp} Regular expression pattern for validating single-line uppercase strings.
      *
      * @public
      * @since 0.1.0
@@ -72,9 +72,9 @@ export class StringUtility {
     /**
      * Get the regular expression for single-line mixed-case strings.
      *
-     * @remarks - This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
+     * @remarks This expression does not allow tab breaks, new lines, leading whitespace, trailing whitespace, or consecutive spaces within the string.
      *
-     * @returns {RegExp} - Regular expression pattern for validating single-line mixed-case strings.
+     * @returns {RegExp} Regular expression pattern for validating single-line mixed-case strings.
      *
      * @public
      * @since 0.1.0
@@ -88,7 +88,7 @@ export class StringUtility {
      *
      * @param {unknown} input - The input to check.
      *
-     * @returns {input is string} - `true` if the given input is a string; `false` otherwise.
+     * @returns {input is string} `true` if the given input is a string; `false` otherwise.
      *
      * @public
      * @since 0.1.0
@@ -103,7 +103,7 @@ export class StringUtility {
      *
      * @param {unknown} input - The input to check.
      *
-     * @returns {input is string} - `true` if the given input is a non-empty string; `false` otherwise.
+     * @returns {input is string} `true` if the given input is a non-empty string; `false` otherwise.
      *
      * @public
      * @since 0.1.0
@@ -119,7 +119,7 @@ export class StringUtility {
      *
      * @param {unknown} input - The input to check.
      *
-     * @returns {input is string} - `true` if the given input is a single-line lowercase string that is trimmed; `false` otherwise.
+     * @returns {input is string} `true` if the given input is a single-line lowercase string that is trimmed; `false` otherwise.
      *
      * @public
      * @since 0.1.0
@@ -135,7 +135,7 @@ export class StringUtility {
      *
      * @param {unknown} input - The input to check.
      *
-     * @returns {input is string} - `true` if the given input is a single-line uppercase string that is trimmed; `false` otherwise.
+     * @returns {input is string} `true` if the given input is a single-line uppercase string that is trimmed; `false` otherwise.
      *
      * @public
      * @since 0.1.0
@@ -151,7 +151,7 @@ export class StringUtility {
      *
      * @param {unknown} input - The input to check.
      *
-     * @returns {input is string} - `true` if the given input is a single-line string that is trimmed; `false` otherwise.
+     * @returns {input is string} `true` if the given input is a single-line string that is trimmed; `false` otherwise.
      *
      * @public
      * @since 0.1.0
