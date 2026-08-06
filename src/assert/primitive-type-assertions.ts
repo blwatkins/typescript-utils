390c66dea4c8644e064b5f628ac4ca324e3456b0
blwatkins <blwatkins@users.noreply.github.com>
2026-08-06 18:01:47 -0500

    Update TSDocs in primitive-type-assertions.ts
    #68 #71

--- src/assert/primitive-type-assertions.ts
+++ src/assert/primitive-type-assertions.ts
@@ -18,10 +18,41 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { PrimitiveTypeError } from '../error';
+import { PrimitiveTypeError, StaticInstanceError } from '../error';
 import { StringUtility } from '../string';
 
+/**
+ * Static methods and properties for validating primitive type values.
+ *
+ * @since 0.1.0
+ */
 export class PrimitiveTypeAssertions {
+    /**
+     * Private constructor.
+     *
+     * @throws {StaticInstanceError} When class is instantiated.
+     * {@link PrimitiveTypeAssertions} is a static class and cannot be instantiated.
+     *
+     * @private
+     */
+    private constructor() {
+        throw new StaticInstanceError('PrimitiveTypeAssertions is a static class and cannot be instantiated.');
+    }
+
+    /**
+     * Validate and assert that the given input is a callable function.
+     *
+     * @remarks This method does not enforce type checking for function parameters or return type.
+     *
+     * @param {unknown} input - The input to check.
+     * @param {string|undefined} message - Optional message for the error thrown if the input is not a function.
+     *
+     * @returns {asserts input is (...args: unknown[]) => unknown} Asserts that the given input is a callable function.
+     *
+     * @throws {PrimitiveTypeError} When the input is not a callable function.
+     *
+     * @since 0.1.0
+     */
     public static assertFunctionType(input: unknown, message?: string): asserts input is (...args: unknown[]) => unknown {
         if (typeof input !== 'function') {
             if (StringUtility.isSingleLineTrimmedString(message)) {
@@ -32,6 +63,20 @@ export class PrimitiveTypeAssertions {
         }
     }
 
+    /**
+     * Validate and assert that the given input is an object.
+     *
+     * @remarks This method does not enforce presence or type checking for any object properties.
+     *
+     * @param {unknown} input - The input to check.
+     * @param {string|undefined} message - Optional message for the error thrown if the input is not an object.
+     *
+     * @returns {asserts input is object} Asserts that the given input is an object.
+     *
+     * @throws {PrimitiveTypeError} When the input is not an object.
+     *
+     * @since 0.1.0
+     */
     public static assertObjectType(input: unknown, message?: string): asserts input is object {
         if (!input || typeof input !== 'object' || Array.isArray(input)) {
             if (StringUtility.isSingleLineTrimmedString(message)) {
