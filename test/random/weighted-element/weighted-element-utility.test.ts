b77c3f8dc6a939d7d60d80c80061a5dd84ef992a
blwatkins <blwatkins@users.noreply.github.com>
2026-08-09 23:06:14 -0500

    Add test cases to weighted-element-utility.test.ts. Add unit tests for asserts methods.
    #68 #71

--- test/random/weighted-element/weighted-element-utility.test.ts
+++ test/random/weighted-element/weighted-element-utility.test.ts
@@ -22,9 +22,11 @@ import { fail } from 'node:assert';
 import { describe, expect, expectTypeOf, test } from 'vitest';
 
 import {
+    PrimitiveTypeError,
+    StringUtility,
     WeightedElement,
     WeightedList,
-    WeightedElementUtility, PrimitiveTypeError, StringUtility
+    WeightedElementUtility, SchemaTypeError
 } from '../../../src';
 
 import { nonArrayInputs } from '../../utils/input/array-inputs';
@@ -117,7 +119,7 @@ describe('WeightedElementUtility', (): void => {
             expected: false
         },
         {
-            label: 'Valid weighted element object',
+            label: 'Valid weighted element objects',
             inputs: [
                 { value: 'hello', weight: 0 },
                 { value: 'hi', weight: 0.5 },
@@ -127,8 +129,99 @@ describe('WeightedElementUtility', (): void => {
         }
     ];
 
-    describe('isGenericWeightedElement()', (): void => {
-        describe('isGenericWeightedElement should correctly identify WeightedElement objects', (): void => {
+    describe('assertGenericWeightedElement', (): void => {
+        describe('Should correctly identify generic WeightedElement objects', (): void => {
+            describe.each(
+                weightedElementInputScenarios
+            )('%# - $label', ({ inputs: scenarioInputs, expected: scenarioExpected }: Scenario): void => {
+                const testCases: TestCase[] = buildTestCases(scenarioInputs, scenarioExpected);
+
+                test.each(
+                    testCases
+                )('%# - Input $input should throw a SchemaTypeError if it is not a valid weighted element', ({ input: testInput, expected: testExpected }: TestCase): void => {
+                    if (testExpected) {
+                        expect((): void => {
+                            WeightedElementUtility.assertGenericWeightedElement(testInput);
+                        }).not.toThrow();
+                    } else {
+                        expect((): void => {
+                            WeightedElementUtility.assertGenericWeightedElement(testInput);
+                        }).toThrow(SchemaTypeError);
+                    }
+                });
+            });
+        });
+    });
+
+    describe('assertWeightedElement', (): void => {
+        const typeGuard: (input: unknown) => input is string = (input: unknown): input is string => {
+            return StringUtility.isSingleLineTrimmedString(input);
+        };
+
+        test('Should assert based on the given type guard and element value', (): void => {
+            const element1: WeightedElement<unknown> = { value: 'single line', weight: 1 };
+            const element2: WeightedElement<unknown> = { value: 'multi\nline', weight: 1 };
+            const element3: WeightedElement<unknown> = { value: 100, weight: 1 };
+
+            expect((): void => {
+                WeightedElementUtility.assertWeightedElement<string>(element1, typeGuard)
+            }).not.toThrow();
+            expect((): void => {
+                WeightedElementUtility.assertWeightedElement<string>(element2, typeGuard)
+            }).toThrow(SchemaTypeError);
+            expect((): void => {
+                WeightedElementUtility.assertWeightedElement<string>(element3, typeGuard)
+            }).toThrow(SchemaTypeError);
+
+        });
+
+        describe('Should throw for any invalid weighted element', (): void => {
+            describe.each(
+                weightedElementInputScenarios
+            )('%# - $label', ({ inputs: scenarioInputs, expected: scenarioExpected }: Scenario): void => {
+                const testCases: TestCase[] = buildTestCases(scenarioInputs, scenarioExpected);
+
+                test.each(
+                    testCases
+                )('%# - Input $input should throw a SchemaTypeError if it is not a valid weighted element', ({ input: testInput, expected: testExpected }: TestCase): void => {
+                    if (!testExpected) {
+                        expect((): void => {
+                            WeightedElementUtility.assertWeightedElement(testInput, typeGuard);
+                        }).toThrow(SchemaTypeError);
+                    }
+                });
+            });
+        });
+
+        describe('Input validation', (): void => {
+            const scenarios: Scenario[] = [
+                {
+                    label: 'Non-function type inputs',
+                    inputs: [
+                        ...nonFunctionInputs
+                    ],
+                    expected: PrimitiveTypeError
+                }
+            ];
+
+            describe.each(
+                scenarios
+            )('%# - $label', ({ inputs: scenarioInputs, expected: scenarioExpected }: Scenario): void => {
+                const testCases: TestCase[] = buildTestCases(scenarioInputs, scenarioExpected);
+
+                test.each(
+                    testCases
+                )('%# - Type guard input $input should throw $expected', ({ input: testInput, expected: testExpected }: TestCase): void => {
+                    expect((): void => {
+                        WeightedElementUtility.assertWeightedElement({}, testInput as ((value: unknown) => value is unknown));
+                    }).toThrow(testExpected);
+                });
+            });
+        });
+    });
+
+    describe('isGenericWeightedElement', (): void => {
+        describe('Should correctly identify WeightedElement objects', (): void => {
             describe.each(
                 weightedElementInputScenarios
             )('%# - $label', ({ inputs: scenarioInputs, expected: scenarioExpected }: Scenario): void => {
